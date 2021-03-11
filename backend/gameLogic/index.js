f69e2eca098f17e528cfd3863c8e4e480f2328b5
Rowan Warneke <rowanwarneke@gmail.com>
2021-03-11 15:37:00 +1100

    built raposci backend

---
+++ backend/gameLogic/index.js
@@ -0,0 +1,432 @@
+const UPPER = "Upper";
+const LOWER = "Lower";
+
+const DRAW = "Draw";
+
+const ACTION_THROW = "throw";
+const ACTION_SLIDE = "slide";
+const ACTION_SWING = "swing";
+
+class Game {
+	static MAX_N_MOVES = 360;
+	static MAX_N_THROWS = 9;
+
+	static NEMISIS = {
+		r: "P",
+		p: "S",
+		s: "R",
+		R: "p",
+		P: "s",
+		S: "r",
+	};
+
+	constructor() {
+		this.board = Game.emptyBoard();
+
+		/*
+		RaPoSci360 is a two player *simultaneous turn* game.
+		
+		We keen track of the submitted turn of each player.
+		If a player submits a turn, check if they are the second to do so.
+		If so, execute both turns. Otherwise, wait for the other player.
+		*/
+		this.nextMoves = {
+			Upper: null,
+			Lower: null,
+		};
+
+		// number of throws taken by each player to this point
+		this.nThrows = {
+			Upper: 0,
+			Lower: 0,
+		};
+
+		// the number of each token type on the total board
+		this.tokenCounts = {
+			r: 0,
+			p: 0,
+			s: 0,
+			R: 0,
+			P: 0,
+			S: 0,
+		};
+
+		// the number of total moves
+		this.nMoves = 0;
+
+		// the number of tokens captured by each player
+		// this could technically be derived from nThrows and tokenCounts
+		// this is really the "score" of the game. First to 9 wins.
+		this.nCaptured = {
+			Upper: 0,
+			Lower: 0,
+		};
+
+		this.gameOver = false;
+		this.winner = null;
+	}
+
+	static emptyBoard() {
+		/*
+		The board is a hexagonal lattice with 5 hexes on each edge (61 total) 
+		The coordinate system uses paramters r and q.
+		- r defines the row, with -4 at the bottom and 4 at the top
+		- q defines the upright diagonal, also from -4 to 4
+		Examples:
+		- the center hex is (0,0)
+		- the leftmost hex is (0,-4)
+		- the bottom right hex is (-4,4)
+		Note that the hexes are not defined by {(r, q) | r in R, q in Q},
+		since for different values of r (q), different values of q (r) exist.
+		We *can* define the board by {(r, q) | r in R, q in Q(r)} to solve this.
+
+		The set Q(r) is defined by
+			{-4-min(r,0), ..., 4-max(r,0)}.
+		I'll just let you think about that one.
+
+		Note that in RaPoSci360, hexes can be occupied by multiple pieces.
+		Pieces must be one of the characters "rpsRPS" (corresponding to rock,
+		paper and scissors of lower and upper respectively).
+
+		The value of each hex is a lookup table of how many of each piece type
+		it contains.
+		*/
+		var board = {};
+		for (let r = -4; r <= 4; r++) {
+			let qmin = -4 - Math.min(r, 0);
+			let qmax = 4 - Math.max(r, 0);
+			for (let q = qmin; q <= qmax; q++) {
+				board[[r, q]] = {
+					r: 0, // this is the name r, not the variable. thank you js
+					p: 0,
+					s: 0,
+					R: 0,
+					P: 0,
+					S: 0,
+				};
+			}
+		}
+		return board;
+	}
+
+	submitMove(move) {
+		if (this.legalMove(move)) {
+			console.log("move accepted");
+			const { player } = move;
+			this.nextMoves[player] = move;
+			if (this.nextMoves.Lower && this.nextMoves.Upper) {
+				this.executeMoves();
+			}
+		} else {
+			console.log("move rejected");
+		}
+	}
+
+	legalMove(move) {
+		const { player, actionType, fromHex, tokenType, toHex } = move;
+		// move must contain certain parameters
+		if (!player || !actionType || !tokenType || !toHex) return false;
+
+		// validate player
+		if (![UPPER, LOWER].includes(player)) return false;
+		// validate action
+		if (![ACTION_THROW, ACTION_SLIDE, ACTION_SWING].includes(actionType))
+			return false;
+		// validate to hex
+		if (!Game.validHex(toHex)) return false;
+		// validate token type
+		if (!Game.validTokenType(tokenType, player)) return false;
+		// validate from hex if necessary
+		if (actionType != ACTION_THROW) {
+			if (!Game.validHex(fromHex)) return false;
+		}
+
+		switch (actionType) {
+			case ACTION_THROW:
+				/*
+				- must have a throw available
+				- must throw onto the first n rows if this is the nth throw
+				*/
+				if (this.nThrows[player] === Game.MAX_N_THROWS) {
+					return false;
+				} else {
+					// this is the nth throw, where n-1 throws have happened
+					var n = this.nThrows[player] + 1;
+				}
+				let r = toHex[0];
+				let row = player === UPPER ? 5 - r : 5 + r;
+				return row <= n;
+			case ACTION_SLIDE:
+				/*
+				- move must be distance 1
+				*/
+				return distanceBetween(fromHex, toHex) === 1;
+			case ACTION_SWING:
+				/*
+				this is super nasty. a swing action is one in which the player
+				"swings" around one of its own pieces. it must start next to
+				a friendly piece, and finish on one of the up-to-three hexes
+				on the *opposite* side of the friendly hex.
+
+				to translate this into our code, note that any swing action
+				necessarily moves the piece a distance of two.
+
+				also notice that the hex about which we swing must be distance
+				one from both the start and end hexes. we will messily implement
+				this by evaluating the up-to-six hexes of distance one from the
+				from hex, then testing each of them to see if 
+				- they are distance one from the to hex; and
+				- they are occupied by a friendly piece.
+				if these conditions are met, the move is legal.
+				*/
+				if (distanceBetween(fromHex, toHex) !== 2) return false;
+				const d1Hexes = Game.hexesOfDistanceOne(fromHex);
+				d1Hexes.forEach((hex) => {
+					if (distanceBetween(hex, toHex) === 1 && occupies(hex, player)) {
+						return true;
+					}
+				});
+				// couldn't find a hex meeting the requirements
+				return false;
+			default:
+				return false;
+		}
+	}
+
+	static distanceBetween(hex1, hex2) {
+		const [r1, q1] = hex1;
+		const [r2, q2] = hex2;
+		const dr = r2 - r1;
+		const dq = q2 - q1;
+		var distance = Math.abs(dr) + Math.abs(dq);
+		// in fact, this will overshoot
+		// we can also take steps "s" direction, which is equivalent to (1,-1).
+		// so we should discount the distance by min(abs(dr), abs(ds)) if
+		// r and s have opposite signs.
+		if (Math.sign(dr) != Math.sign(dq)) {
+			const discount = Math.min(Math.abs(dr), Math.abs(dq));
+			distance -= discount;
+		}
+		return distance;
+	}
+
+	static hexesOfDistanceOne(fromHex) {
+		const [r, q] = fromHex;
+		return [
+			[r + 1, q],
+			[r + 1, q - 1],
+			[r, q - 1],
+			[r, q + 1],
+			[r - 1, q],
+			[r - 1, q + 1],
+		].filter((hex) => Game.validHex(hex));
+	}
+
+	static validHex(hex) {
+		if (!hex.length || hex.length !== 2) return false;
+		const [r, q] = hex;
+		if (!Number.isInteger(r) || !Number.isInteger(q)) return false;
+		if (r < -4 || r > 4) return false;
+		let qmin = -4 - Math.min(r, 0);
+		let qmax = 4 - Math.max(r, 0);
+		if (q < qmin || q > qmax) return false;
+		return true;
+	}
+
+	static validTokenType(tokenType, player) {
+		if (player === UPPER) {
+			return "RPS".includes(tokenType);
+		} else if (player === LOWER) {
+			return "rps".includes(tokenType);
+		}
+		return false;
+	}
+
+	invincible(player) {
+		// test whether a player has invincible tokens
+		const otherPlayer = Game.otherPlayer(player);
+		if (this.nThrows[otherPlayer] < Game.MAX_N_THROWS) {
+			// other player still has throws left!
+			return false;
+		}
+		const ourTokenTypes = player === UPPER ? "RPS" : "rps";
+		ourTokenTypes.forEach((tt) => {
+			if (this.tokenCounts[tt]) {
+				// we have one of these tokens. can it be defeated?
+				var nemisis = Game.NEMISIS[tt];
+				if (!this.tokenCounts[nemisis]) {
+					// no! it can't!
+					return true;
+				}
+			}
+		});
+		// couldn't find an invincible token.
+		return false;
+	}
+
+	static otherPlayer(player) {
+		return player === UPPER ? LOWER : UPPER;
+	}
+
+	executeMoves() {
+		console.log("executing...");
+		// move the tokens
+		var toHexes = [];
+		[this.nextMoves.Lower, this.nextMoves.Upper].forEach((move) => {
+			console.log(move);
+			// the move is assumed to be legal
+			const { player, actionType, fromHex, tokenType, toHex } = move;
+			this.board[toHex][tokenType] += 1;
+			// keep track of the toHexes for later
+			toHexes.push(toHex);
+			if (actionType == ACTION_THROW) {
+				this.nThrows[player]++;
+				this.tokenCounts[tokenType]++;
+			} else {
+				this.board[fromHex][tokenType] -= 1;
+			}
+		});
+
+		// capture tokens
+		// we only need to check the toHexes
+		toHexes.forEach((toHex) => {
+			var defeated = Game.battle(this.board[toHex]);
+			for (let tokenType in defeated) {
+				this.tokenCounts[tokenType] -= defeated[tokenType];
+				if (tokenType === tokenType.toUpperCase()) {
+					// token is upper, award captures to loewr
+					this.nCaptured.Lower += defeated[tokenType];
+				} else {
+					// award captures to upper
+					this.nCaptured.Upper += defeated[tokenType];
+				}
+			}
+		});
+
+		// acknowledge the increase in moves (for draw-checking purposes)
+		this.nMoves++;
+
+		this.nextMoves.Upper = this.nextMoves.Lower = null;
+
+		// check endgame
+
+		/*
+		1: One player has no remaining throws and all of their tokens have been
+		defeated: If the other player still has tokens or throws, declare that
+		player the winner. Otherwise, declare a draw.
+
+		This is equivalent to just checking for nCaptured == MAX_N_THROWS
+		*/
+		if (
+			this.nCaptured.Upper === Game.MAX_N_THROWS &&
+			this.nCaptured.Lower === Game.MAX_N_THROWS
+		) {
+			self.gameOver = true;
+			self.winner = DRAW;
+			return;
+		}
+		[UPPER, LOWER].forEach((player) => {
+			if (this.nCaptured[player] === Game.MAX_N_THROWS) {
+				self.gameOver = true;
+				self.winner = player;
+				return;
+			}
+		});
+
+		/*
+		2: A token is *invincible* if it cannot be defeated by the opponent's
+		remaining tokens, and the opponent has no remaining throws. Both players
+		have an invincible token: Declare a draw.
+		*/
+		if (this.invincible(UPPER) && this.invincible(LOWER)) {
+			self.gameOver = true;
+			self.winner = DRAW;
+			return;
+		}
+
+		/*
+		3: One player has an invincible token an the other player has only one
+		remaining token (not invincible): Declare the player with the invincible
+		token the winner.
+		*/
+		[UPPER, LOWER].forEach((player) => {
+			if (this.invincible(player)) {
+				// does the other player have only one token left?
+				if (this.nCaptured[player] === Game.MAX_N_THROWS - 1) {
+					// yes!
+					self.gameOver = true;
+					self.winner = player;
+					return;
+				}
+			}
+		});
+
+		/*
+		4: One game configuration (with the same number of tokens with each
+		symbol and controlling player occupying each hex, and the same number
+		of throws remaining for each player), occurs for a third time since the
+		start of the game (not necessarily in succession): Declare a draw.
+		*/
+
+		/*
+		5: The players have had their 360th turn without a winner being 
+		declared: declare a draw.
+		*/
+		if (this.nMoves >= Game.MAX_N_MOVES) {
+			self.gameOver = true;
+			self.winner = DRAW;
+			return;
+		}
+	}
+
+	static battle(hex) {
+		var hasR = hex.r || hex.R;
+		var hasP = hex.p || hex.P;
+		var hasS = hex.s || hex.S;
+		const defeated = {
+			r: 0,
+			R: 0,
+			p: 0,
+			P: 0,
+			s: 0,
+			S: 0,
+		};
+		if (hasR) {
+			// rock defeats scissors
+			defeated.s = hex.s;
+			defeated.S = hex.S;
+			hex.s = hex.S = 0;
+		}
+		if (hasP) {
+			// paper defeats rock somehow
+			defeated.r = hex.r;
+			defeated.R = hex.R;
+			hex.r = hex.R = 0;
+		}
+		if (hasS) {
+			// scissors defeats paper
+			defeated.p = hex.p;
+			defeated.P = hex.P;
+			hex.p = hex.P = 0;
+		}
+		return defeated;
+	}
+}
+
+game = new Game();
+
+move1 = {
+	player: "Upper",
+	actionType: "throw",
+	tokenType: "S",
+	toHex: [4, -2],
+};
+game.submitMove(move1);
+
+move2 = {
+	player: "Lower",
+	actionType: "throw",
+	tokenType: "r",
+	toHex: [-4, 1],
+};
+game.submitMove(move2);
